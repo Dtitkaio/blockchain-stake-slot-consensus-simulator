1934521077c9aff7462aff2bef003c1b7eeb7a83
Kai Otsuki <kai.ootsuki.su@nttdocomo.com>
2024-02-27 11:46:16 +0900

    feat
    Consts

---
+++ config.py
@@ -0,0 +1,16 @@
+# config.py
+
+# Initial stakes for each validator
+STAKE_DISTRIBUTION = [100, 200, 300, 400, 500]
+
+# Number of epochs to simulate
+EPOCHS = 10
+
+# Number of authority validators selected each epoch
+AUTHORITY_COUNT = 3
+
+# Reward distribution ratio between authority and candidate validators
+REWARD_RATIO = [0.8, 0.2]
+
+# Total reward distributed per epoch
+TOTAL_REWARD = 10
